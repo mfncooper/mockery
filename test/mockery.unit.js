d6584cfc3c42dcd393c5cc3f84ffa38bef2eee4e
Bryan Donovan <bdondo@gmail.com>
2011-11-06 21:19:56 -0800

    adding initial functional test

--- test/mockery.unit.js
+++ test/mockery.unit.js
@@ -1,16 +1,12 @@
 /*
  * Run with nodeunit:
- *   nodeunit mockery.unit.js
+ *   nodeunit --reporter nested mockery.unit.js
  */
 var testCase = require('nodeunit').testCase;
 var mockery = require('../mockery');
 var sinon = require('sinon');
 
 module.exports = testCase({
-    setUp: function(callback) {
-        callback();
-    },
-        
     tearDown: function (callback) {
         mockery.disable();
         mockery.deregisterAll();
@@ -21,8 +17,8 @@ module.exports = testCase({
         "module not yet registered": testCase({
             "adds module path to registeredMocks": function(test) {
                 var mock_foo = {'fake': 'foo'};
-                mockery.registerMock('./fake_module', mock_foo);
-                test.deepEqual(mockery.getRegisteredMocks()['./fake_module'], mock_foo);
+                mockery.registerMock('./support/fake_module', mock_foo);
+                test.deepEqual(mockery.getRegisteredMocks()['./support/fake_module'], mock_foo);
                 test.done();
             }
         }),
@@ -30,16 +26,16 @@ module.exports = testCase({
         "module already registered": testCase({
             "adds module path to registeredMocks, logs warning": function(test) {
                 var mock_foo = {'fake': 'foo'};
-                mockery.registerMock('./fake_module', mock_foo);
+                mockery.registerMock('./support/fake_module', mock_foo);
 
                 var mock_console = sinon.mock(console);
                 mock_console.expects('warn').once();
 
-                mockery.registerMock('./fake_module', mock_foo);
+                mockery.registerMock('./support/fake_module', mock_foo);
 
                 mock_console.verify();
                 mock_console.restore();
-                test.deepEqual(mockery.getRegisteredMocks()['./fake_module'], mock_foo);
+                test.deepEqual(mockery.getRegisteredMocks()['./support/fake_module'], mock_foo);
                 test.done();
             }
         })
@@ -48,9 +44,9 @@ module.exports = testCase({
     "deregisterMock": testCase({
         "removes module from registeredMocks": function(test) {
             var mock_foo = {'fake': 'foo'};
-            mockery.registerMock('./fake_module', mock_foo);
-            mockery.deregisterMock('./fake_module');
-            test.equal(typeof mockery.getRegisteredMocks()['./fake_module'], 'undefined');
+            mockery.registerMock('./support/fake_module', mock_foo);
+            mockery.deregisterMock('./support/fake_module');
+            test.equal(typeof mockery.getRegisteredMocks()['./support/fake_module'], 'undefined');
             test.done();
         }
     }),
@@ -58,26 +54,26 @@ module.exports = testCase({
     "registerSubstitute": testCase({
         "module not yet registered": testCase({
             "adds module path to registeredSubstitutes": function(test) {
-                var substitute_path = './fake_module_sub';
-                mockery.registerSubstitute('./fake_module', substitute_path);
-                test.deepEqual(mockery.getRegisteredSubstitutes()['./fake_module'], substitute_path);
+                var substitute_path = './support/fake_module_sub';
+                mockery.registerSubstitute('./support/fake_module', substitute_path);
+                test.deepEqual(mockery.getRegisteredSubstitutes()['./support/fake_module'], substitute_path);
                 test.done();
             }
         }),
 
         "module already registered": testCase({
             "adds module path to registeredSubstitutes, logs warning": function(test) {
-                var substitute_path = './fake_module_sub';
-                mockery.registerSubstitute('./fake_module', substitute_path);
+                var substitute_path = './support/fake_module_sub';
+                mockery.registerSubstitute('./support/fake_module', substitute_path);
 
                 var mock_console = sinon.mock(console);
                 mock_console.expects('warn').once();
 
-                mockery.registerSubstitute('./fake_module', substitute_path);
+                mockery.registerSubstitute('./support/fake_module', substitute_path);
 
                 mock_console.verify();
                 mock_console.restore();
-                test.deepEqual(mockery.getRegisteredSubstitutes()['./fake_module'], substitute_path);
+                test.deepEqual(mockery.getRegisteredSubstitutes()['./support/fake_module'], substitute_path);
                 test.done();
             }
         })
@@ -85,10 +81,10 @@ module.exports = testCase({
 
     "deregisterSubstitute": testCase({
         "removes module from registeredSubstitutes": function(test) {
-            var substitute_path = './fake_module_sub';
-            mockery.registerSubstitute('./fake_module', substitute_path);
-            mockery.deregisterSubstitute('./fake_module');
-            test.equal(typeof mockery.getRegisteredSubstitutes()['./fake_module'], 'undefined');
+            var substitute_path = './support/fake_module_sub';
+            mockery.registerSubstitute('./support/fake_module', substitute_path);
+            mockery.deregisterSubstitute('./support/fake_module');
+            test.equal(typeof mockery.getRegisteredSubstitutes()['./support/fake_module'], 'undefined');
             test.done();
         }
     }),
@@ -96,16 +92,16 @@ module.exports = testCase({
     "registerAllowable": testCase({
         "without unhook param": testCase({
             "adds module to registeredAllowables, sets unhook to false": function(test) {
-                mockery.registerAllowable('./fake_module');
-                test.deepEqual(mockery.getRegisteredAllowables()['./fake_module'], {unhook: false, paths: []});
+                mockery.registerAllowable('./support/fake_module');
+                test.deepEqual(mockery.getRegisteredAllowables()['./support/fake_module'], {unhook: false, paths: []});
                 test.done();
             }
         }),
 
         "with unhook param": testCase({
             "adds module to registeredAllowables, sets unhook to true": function(test) {
-                mockery.registerAllowable('./fake_module', true);
-                test.deepEqual(mockery.getRegisteredAllowables()['./fake_module'], {unhook: true, paths: []});
+                mockery.registerAllowable('./support/fake_module', true);
+                test.deepEqual(mockery.getRegisteredAllowables()['./support/fake_module'], {unhook: true, paths: []});
                 test.done();
             }
         })
@@ -113,37 +109,37 @@ module.exports = testCase({
 
     "deregisterAllowable": testCase({
         "removes module from registeredAllowables": function(test) {
-            mockery.registerAllowable('./fake_module');
-            mockery.deregisterAllowable('./fake_module');
-            test.equal(typeof mockery.getRegisteredAllowables()['./fake_module'], 'undefined');
+            mockery.registerAllowable('./support/fake_module');
+            mockery.deregisterAllowable('./support/fake_module');
+            test.equal(typeof mockery.getRegisteredAllowables()['./support/fake_module'], 'undefined');
             test.done();
         }
     }),
 
     "deregisterAll": testCase({
         setUp: function(callback) {
-            var substitute_path = './fake_module_sub';
+            var substitute_path = './support/fake_module_sub';
             var mock_foo = {'fake': 'foo'};
-            mockery.registerMock('./fake_module', mock_foo);
-            mockery.registerSubstitute('./fake_module', substitute_path);
-            mockery.registerAllowable('./fake_module');
+            mockery.registerMock('./support/fake_module', mock_foo);
+            mockery.registerSubstitute('./support/fake_module', substitute_path);
+            mockery.registerAllowable('./support/fake_module');
 
-            mockery.deregisterAll('./fake_module');
+            mockery.deregisterAll('./support/fake_module');
             callback();
         },
 
         "removes modules from registeredMocks": function(test) {
-            test.equal(typeof mockery.getRegisteredMocks()['./fake_module'], 'undefined');
+            test.equal(typeof mockery.getRegisteredMocks()['./support/fake_module'], 'undefined');
             test.done();
         },
 
         "removes modules from registeredSubstitutes": function(test) {
-            test.equal(typeof mockery.getRegisteredSubstitutes()['./fake_module'], 'undefined');
+            test.equal(typeof mockery.getRegisteredSubstitutes()['./support/fake_module'], 'undefined');
             test.done();
         },
 
         "removes modules from registeredAllowables": function(test) {
-            test.equal(typeof mockery.getRegisteredAllowables()['./fake_module'], 'undefined');
+            test.equal(typeof mockery.getRegisteredAllowables()['./support/fake_module'], 'undefined');
             test.done();
         }
     })
